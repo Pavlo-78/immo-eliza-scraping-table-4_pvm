3596efcaa419aac115085de807bad1d08daa0409
Pavlo_Miroshnyk <pv.miroshnyk@gmail.com>
2023-10-07 15:10:20 +0200

    Well working, OOP + data in two places

--- utilsOOP/class3_wp_property.py
+++ utilsOOP/class3_wp_property.py
@@ -12,8 +12,7 @@ class wpage_property(c2.wpage_links):
     def __init__(self, desc, url, no):
         super().__init__(desc, url)
         self.no = no 
-        # self.Postal_code = None
-
+        self.ddict = {}
 
     def get_data_immoweb(self):
         #
@@ -36,102 +35,122 @@ class wpage_property(c2.wpage_links):
         self.row_data = json.loads(json_text1) #print(type(json_data)) >> <class 'dict'>
       
         try:
-            self.PropertyId = self.row_data["id"]            
+            self.ddict["id"]    = self.row_data["id"]
+            self.PropertyId     = self.row_data["id"]
         except (KeyError, TypeError):
             self.PropertyId = None
 
-        try:
-            self.Locality_Name = self.row_data["property"]["location"]["locality"]
+        try:          
+            self.ddict["Locality_Name"] = self.row_data["property"]["location"]["locality"]
+            self.Locality_Name          = self.row_data["property"]["location"]["locality"]
         except (KeyError, TypeError):
             self.Locality_Name = None
 
         try:
-            self.Postal_code = self.row_data["property"]["location"]["postalCode"]
+            self.ddict["Postal_code"]   = self.row_data["property"]["location"]["postalCode"]
+            self.Postal_code            = self.row_data["property"]["location"]["postalCode"]
         except (KeyError, TypeError):
             self.Postal_code = None
 
         try:
-            self.Type_of_property = self.row_data["property"]["type"]
+            self.ddict["Type_of_property"]  = self.row_data["property"]["type"]
+            self.Type_of_property           = self.row_data["property"]["type"]
         except (KeyError, TypeError):
             self.Type_of_property = None
 
         try:
-            self.Subtype_of_property = self.row_data["property"]["subtype"]
+            self.ddict["Subtype_of_property"]   = self.row_data["property"]["subtype"]
+            self.Subtype_of_property            = self.row_data["property"]["subtype"]
         except (KeyError, TypeError):
             self.Subtype_of_property = None
 
         try:
-            self.Price = self.row_data["transaction"]["sale"]["price"]
+            self.ddict["Price"] = self.row_data["transaction"]["sale"]["price"]
+            self.Price          = self.row_data["transaction"]["sale"]["price"]
         except (KeyError, TypeError):
             self.Price = None
 
         try:
-            self.Type_of_sale = self.row_data["transaction"]["type"]
+            self.ddict["Type_of_sale"]  = self.row_data["transaction"]["type"]
+            self.Type_of_sale           = self.row_data["transaction"]["type"]
         except (KeyError, TypeError):
             self.Type_of_sale = None
 
         try:
-            self.Number_of_rooms = self.row_data["property"]["bedroomCount"]
+            self.ddict["Number_of_rooms"]   = self.row_data["property"]["bedroomCount"]
+            self.Number_of_rooms            = self.row_data["property"]["bedroomCount"]
         except (KeyError, TypeError):
             self.Number_of_rooms = None
 
         try:
-            self.Living_Area = self.row_data["property"]["netHabitableSurface"]
+            self.ddict["Living_Area"]   = self.row_data["property"]["netHabitableSurface"]
+            self.Living_Area            = self.row_data["property"]["netHabitableSurface"]
         except (KeyError, TypeError):
             self.Living_Area =None
 
         try:    
-            self.Equipped_kitchen = self.row_data["property"]["kitchen"]["type"]
+            self.ddict["Equipped_kitchen"]  = self.row_data["property"]["kitchen"]["type"]
+            self.Equipped_kitchen           = self.row_data["property"]["kitchen"]["type"]
         except (KeyError, TypeError):   
             self.Equipped_kitchen = None
 
         try:
-            self.Furnished = self.row_data["transaction"]["sale"]["isFurnished"]
+            self.ddict["Furnished"] = self.row_data["transaction"]["sale"]["isFurnished"]
+            self.Furnished          = self.row_data["transaction"]["sale"]["isFurnished"]
         except (KeyError, TypeError):
             self.Furnished = None
 
         try:
-            self.Open_fire = self.row_data["property"]["fireplaceExists"]
+            self.ddict["Open_fire"] = self.row_data["property"]["fireplaceExists"]
+            self.Open_fire          = self.row_data["property"]["fireplaceExists"]
         except (KeyError, TypeError):
             self.Open_fire = None
 
         try:
-            self.Terrace_YN = self.row_data["property"]["hasTerrace"]
+            self.ddict["Terrace_YN"]    = self.row_data["property"]["hasTerrace"]
+            self.Terrace_YN             = self.row_data["property"]["hasTerrace"]
         except (KeyError, TypeError):
             self.Terrace_YN = None
 
         try:
-            self.TerraceSurface = self.row_data["property"]["hasTerrace"]
+            self.ddict["TerraceSurface"]    = self.row_data["property"]["hasTerrace"]
+            self.TerraceSurface             = self.row_data["property"]["hasTerrace"]
         except (KeyError, TypeError):
             self.TerraceSurface = None
 
         try:
-            self.Garden_YN = self.row_data["property"]["hasGarden"]
+            self.ddict["Garden_YN"] = self.row_data["property"]["hasGarden"]
+            self.Garden_YN          = self.row_data["property"]["hasGarden"]
         except (KeyError, TypeError):
             self.Garden_YN = None
 
         try:
-            self.Garden_area = self.row_data["property"]["gardenSurface"]
+            self.ddict["Garden_area"]   = self.row_data["property"]["gardenSurface"]
+            self.Garden_area            = self.row_data["property"]["gardenSurface"]
         except (KeyError, TypeError):
             self.Garden_area = None
 
         try:
-            self.Surface_of_good = self.row_data["property"]["building"]["streetFacadeWidth"]
+            self.ddict["Surface_of_good"]   = self.row_data["property"]["building"]["streetFacadeWidth"]
+            self.Surface_of_good            = self.row_data["property"]["building"]["streetFacadeWidth"]
         except (KeyError, TypeError):
             self.Surface_of_good = None
 
         try:
-            self.Number_of_facades = self.row_data["property"]["building"]["facadeCount"]
+            self.ddict["Number_of_facades"] = self.row_data["property"]["building"]["facadeCount"]
+            self.Number_of_facades          = self.row_data["property"]["building"]["facadeCount"]
         except (KeyError, TypeError):
             self.Number_of_facades = None
 
         try:
-            self.Swimming_pool_YN = self.row_data["property"]["hasSwimmingPool"]
+            self.ddict["Swimming_pool_YN"]  = self.row_data["property"]["hasSwimmingPool"]
+            self.Swimming_pool_YN           = self.row_data["property"]["hasSwimmingPool"]
         except (KeyError, TypeError):
             self.Swimming_pool_YN = None
 
         try:
-            self.State_of_building = self.row_data["property"]["building"]["condition"]
+            self.ddict["State_of_building"] = self.row_data["property"]["building"]["condition"]
+            self.State_of_building          = self.row_data["property"]["building"]["condition"]
         except (KeyError, TypeError):
             self.State_of_building = None
 
--- utilsOOP/main.py
+++ utilsOOP/main.py
@@ -22,41 +22,46 @@ ooc_wpage_searchsale = c2.wpage_links("webpage with all objects for SALE",
 
 
 # getting links
-print(datetime.now().strftime("%d/%m/%Y %H:%M:%S"),"=", 'Collecting links is started...')
-lc=msg=ooc_wpage_searchsale.get_links(500)
-time=datetime.now().strftime("%d/%m/%Y %H:%M:%S")
-msg1="Collecting links is finished."
-msg2="links are collected and saved to json."
-msg3= f"{time} {msg1} {lc} {msg2}"
-print(msg3)
+# print(datetime.now().strftime("%d/%m/%Y %H:%M:%S"),"=", 'Collecting links is started...')
+# lc=msg=ooc_wpage_searchsale.get_links(500)
+# time=datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+# msg1="Collecting links is finished."
+# msg2="links are collected and saved to json."
+# msg3= f"{time} {msg1} {lc} {msg2}"
+# print(msg3)
 
-
+#-- ПОСИЛАННЯ
+# -----------
 # open json file with urls 
 with open('web_urls_.json', encoding='utf-8') as f: 
     # load json to (jurls) list of dictionaries with urls,no,num of page...
     jurls = json.load(f) 
 
-
+#-- ОБ'ЄКТИ
+# -----------
 alldata_lst=[]
+alldata_lst2=[]
 pp=[]
 # create immo objects by cycle each url-dict to extract url & no. "iobj" - object of immo property
-# for jurl in (jurls[0:350]): #dbg
-for jurl in (jurls):
+for jurl in (jurls[0:2]): #dbg
+# for jurl in (jurls):
     ob=f"iobj{jurl['No']}"
     no=jurl['No']
     ur=jurl["url"]
     pp.append(c3.wpage_property(ob, ur, no))
 
-#
+# ЗАВАНТАЖЕННЯ ІНФО В ОБ'ЄКТИ
 # handeling all immo_oblects
 for p in(pp):
-    # get&set data to obj
-    if p.get_data_immoweb(): # start and feedback getting data
+    # get & set data to obj + feedback
+    if p.get_data_immoweb()==True: # 
         # log_msg
         if int(p.no)%100==0: 
             log_msg = datetime.now().strftime("%d/%m/%Y %H:%M:%S") +"=" + p.no + 'links are proceeded'
             print(log_msg)            
-    else: continue # if it run into bad link  
+    else: 
+            print("bad link=",p.no)
+            continue # if it run into bad link  
 
     # reading all attibuties names from object 
     att=['no', 'url', 'PropertyId', 'Locality_Name', 'Postal_code', 'Type_of_property', 
@@ -75,16 +80,18 @@ for p in(pp):
         obj_data_dict[vn] = getattr(p, vn)
     #  add record in list for CSV    
     alldata_lst.append(obj_data_dict)
+    alldata_lst2.append(p.ddict)
+print(alldata_lst2)
 
 
 # preparing save dict to CSV 
-field_names = alldata_lst[0].keys() # get all keys od dict for csv.DictWriter
+field_names = alldata_lst2[0].keys() # get all keys od dict for csv.DictWriter
 # print(field_names)
 # writing dict to CSV
 with open('web_urls_data.csv', 'w', newline='', encoding='utf-8') as csvfile:
     writer = csv.DictWriter(csvfile, fieldnames=field_names, quoting=csv.QUOTE_ALL)
     writer.writeheader()
-    writer.writerows(alldata_lst)
+    writer.writerows(alldata_lst2)
 
 
 print(datetime.now().strftime("%d/%m/%Y %H:%M:%S"), "= The scrapping is fifnished.")
